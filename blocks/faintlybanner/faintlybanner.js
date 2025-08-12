5d61caabdfc8d4d13c14d9f77b6eec04cd416460
Snehil Gupta <Snehil.Gupta1@gds.ey.com>
2025-08-12 18:05:28 +0530

    faintly banner component updated

--- blocks/faintlybanner/faintlybanner.js
+++ blocks/faintlybanner/faintlybanner.js
@@ -1,34 +1,14 @@
 import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js';
+import { renderBlock } from '../../scripts/faintly.js';
+
 
 export default async function decorate(block) {
-  const bannerVariant = block?.children[2]?.innerText?.trim();
 
-  const imgEl = block.children[0];
-  const img = document.createElement('img');
-  img.src = imgEl?.querySelector('img')?.getAttribute('src');
-  img.alt = imgEl?.querySelector('img')?.getAttribute('alt');
-  img.className = 'banner-image';
-
-  const text = block.children[1].innerText.trim();
-
-  const textDiv = document.createElement('div');
-  textDiv.className = 'banner-text';
-  textDiv.innerHTML = text;
-
-  block.textContent = '';
-  if (bannerVariant === 'lirt') {
-    block.append(img, textDiv);
-  } else {
-    block.append(textDiv, img);
-  }
-
-  const language = getMetadata('language');
-  const placeholders = await fetchPlaceholders(language);
-  const { name } = placeholders;
-
-  const placeholderDiv = document.createElement('p');
-  placeholderDiv.className = 'placeholder-text';
-  placeholderDiv.innerHTML = `<p>${name}</p>`;
-
-  textDiv.appendChild(placeholderDiv);
+ await renderBlock(block, {
+    imgsrc: block.children[0]?.querySelector('img')?.getAttribute('src'),
+    imgalt: block.children[0]?.querySelector('img')?.getAttribute('alt'),
+    text: block.children[1]?.innerText?.trim(),
+    bannervariant: block?.children[2]?.innerText?.trim(),
+    test :  (context) => context.bannervariant === "rilt",
+  });
 }
