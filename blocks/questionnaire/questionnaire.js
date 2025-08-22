04813db3f4f6eace23a1f23465779812671b590f
Snehil Gupta <Snehil.Gupta1@gds.ey.com>
2025-08-22 11:39:14 +0530

    updated component

--- blocks/questionnaire/questionnaire.js
+++ blocks/questionnaire/questionnaire.js
@@ -1,5 +1,12 @@
 import { renderBlock } from '../../scripts/faintly.js';
 
 export default async function decorate(block) {
-  await renderBlock(block);
+  await renderBlock(block,{
+    title: block.children[0]?.innerText?.trim(),
+    description: block.children[1]?.innerText?.trim(),
+    question: block.children[2]?.innerText?.trim(),
+    options: Array.from(block.children[3]?.children || []).map(option => option.innerText.trim()),
+    submitText: block.children[4]?.innerText?.trim(),
+    test: (context) => context.title && context.description && context.question && context.options.length > 0,
+  });
 }
